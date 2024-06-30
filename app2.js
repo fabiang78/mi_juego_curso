6e3e6eb8c94c2cf029e527913e0cd5d7113510c7
fabiang78 <105511071+fabiang78@users.noreply.github.com>
2024-06-30 19:02:38 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,13 @@
+let listaGenertica = [];
+console.log(listaGenertica);
+
+let lenguajesProgramacion = ['Javascript', 'C','C++', 'Kotlin', 'Python']
+console.log(lenguajesProgramacion);
+
+lenguajesProgramacion.push ('Ruby', 'Goland');
+console.log(lenguajesProgramacion);
+
+function mostrarLenguajesProgramacion() {
+    console.log(lenguajesProgramacion);
+}
+mostrarLenguajesProgramacion();
